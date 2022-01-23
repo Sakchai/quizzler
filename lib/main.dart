979dc52977c77b14e26d94f0545b9691225302d1
Sakchai Thanasamut <sakchai.th@prtr.com>
2022-01-23 13:32:51 +0700

    initial project

---
+++ lib/main.dart
@@ -0,0 +1,141 @@
+import 'package:flutter/material.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+import 'quiz_brain.dart';
+
+QuizBrain quizBrain = QuizBrain();
+
+void main() => runApp(Quizzler());
+
+class Quizzler extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                quizBrain.getQuestionText(),
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              textColor: Colors.white,
+              color: Colors.green,
+              child: Text(
+                'True',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: () {
+                checkAnswer(true);
+                //The user picked true.
+              },
+            ),
+          ),
+        ),
+        buildExpanded(),
+        //TODO: Add a Row here as your score keeper
+        Row(
+          children: scoreKeeper,
+        ),
+      ],
+    );
+  }
+
+
+
+  Expanded buildExpanded() {
+    return Expanded(
+        child: Padding(
+          padding: EdgeInsets.all(15.0),
+          child: FlatButton(
+            color: Colors.red,
+            child: Text(
+              'False',
+              style: TextStyle(
+                fontSize: 20.0,
+                color: Colors.white,
+              ),
+            ),
+            onPressed: () {
+              //The user picked false.
+              checkAnswer(false);
+            },
+          ),
+        ),
+      );
+  }
+
+  void checkAnswer(bool answer) {
+    bool correctAnswer = quizBrain.getCorrectAnswer();
+    setState(() {
+      if (correctAnswer == answer) {
+        scoreKeeper.add(Icon(Icons.check,color: Colors.green));
+      } else {
+        scoreKeeper.add(Icon(Icons.close,color: Colors.red));
+      }
+      if (quizBrain.isFinished()) {
+        Alert(
+          context: context,
+          type: AlertType.info,
+          title: 'Finished!',
+          desc: 'You\'ve reached the end of the quiz.',
+          buttons: [
+            DialogButton(
+              child: Text(
+                "OK",
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+              onPressed: () => Navigator.pop(context),
+              color: Color.fromRGBO(0, 179, 134, 1.0),
+              radius: BorderRadius.circular(0.0),
+            ),
+          ],
+        ).show();
+        quizBrain.reset();
+        scoreKeeper = [];
+      }
+      quizBrain.nextQuestion();
+    });
+  }
+}
---
+++ lib/question.dart
@@ -0,0 +1,9 @@
+class Question {
+  late String questionText;
+  late bool questionAnswer;
+
+  Question(String q, bool a) {
+    this.questionText = q;
+    this.questionAnswer = a;
+  }
+}
